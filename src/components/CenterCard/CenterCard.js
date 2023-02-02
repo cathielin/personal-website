133df50fad6659e5f23abedc1b73c05d7fb2fb2b
Cathie Lin <cathielin@berkeley.edu>
2023-02-01 18:02:11 -0800

    Added modal CSS transition

--- src/components/CenterCard/CenterCard.js
+++ src/components/CenterCard/CenterCard.js
@@ -4,12 +4,15 @@ import me from '../../images/icon-images/me.jpg';
 function CenterCard() {
     return (
         <div className="card">
+            
                 <div className="card-header">
                     cathie lin
                 </div>
                 <div className="card-content">
                     <img className="me-image" src={me} alt="me"/>
                 </div>
+                
+            
         </div>
     )
 }
